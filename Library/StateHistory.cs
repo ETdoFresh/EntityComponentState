b12976cc5a87e2c12abf3a170d888a0073163016
ETdoFresh <ETdoFresh@gmail.com>
2020-02-14 09:18:37 -0600

    Create StateHistory

---
+++ Library/StateHistory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EntityComponentState
+{
+    public class StateHistory : List<State>
+    {
+        public int LatestTick => this.Max(state => state.tick);
+        public State LatestState => this.FirstOrDefault(state => state.tick == LatestTick);
+
+        public State GetState(int tick)
+        {
+            return this.Where(state => state.tick == tick).FirstOrDefault();
+        }
+
+        public DeltaState GetDeltaState(int startTick, int endTick)
+        {
+            var startState = GetState(startTick);
+            var endState = GetState(endTick);
+            return new DeltaState(startState, endState);
+        }
+
+        public DeltaState GetDeltaState(int startTick)
+        {
+            return GetDeltaState(startTick, LatestTick);
+        }
+    }
+}
